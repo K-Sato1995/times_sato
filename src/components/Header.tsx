5e39d2532653d9cbf6dde0a36e063e414f988611
Katsuki <Katsuki_Sato0130@icloud.com>
2021-04-20 10:23:25 +0900

    Init

---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const NavWrapper = styled.div`
+  margin: 0 auto;
+  height: 50px;
+  border-bottom: solid #e0e0e0 1px;
+`
+
+const Nav = styled.div`
+  margin: 0 auto;
+  max-width: 860px;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+
+  /* border: solid 1px; */
+`
+
+const Title = styled.h1`
+  font-size: 1.2em;
+  color: 000000;
+  padding-left: 20px;
+  /* border: solid 1px; */
+`
+
+const Header = () => {
+  return (
+    <NavWrapper>
+      <Nav>
+        <Title>times_sato</Title>
+      </Nav>
+    </NavWrapper>
+  )
+}
+
+export default Header
---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.div`
+  max-width: 860px;
+  margin: 0 auto;
+  margin-top: 20px;
+  border: solid 1px;
+`
+
+const Main = () => {
+  return <MainContainer></MainContainer>
+}
+
+export default Main
