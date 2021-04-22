287a4637e2b1aebfaa748cf1313d874e89773eb7
Katsuki <Katsuki_Sato0130@icloud.com>
2021-04-22 17:02:00 +0900

    Use react linkify instead

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { ReactTinyLink } from 'react-tiny-link'
 import styled from 'styled-components'
 import { format } from 'date-fns'
 import { firebase } from 'firebaseConfig'
+import Linkify from 'react-linkify'
 
 const CommentWrapper = styled.div`
   position: relative;
@@ -11,44 +11,31 @@ const CommentWrapper = styled.div`
   padding-top: 30px;
   padding-bottom: 20px;
   margin: 25px;
+  /* border: solid 1px; */
 
-  > .react_tinylink_card {
-    box-shadow: none;
+  > a {
+    color: #2c7b7d;
+    text-decoration: none;
+
+    :hover {
+      text-decoration: underline;
+    }
   }
 `
 
 const PostedDate = styled.span`
   position: absolute;
   color: #697980;
-  right: 0;
+  left: 0;
   top: 0;
 `
-
-const Text = styled.p``
 
 interface Props {
   text: string
   createdAt: firebase.firestore.Timestamp
 }
 
-function isValidWebUrl(url: string) {
-  let regEx = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_.~#?&//=]*)$/gm
-  return regEx.test(url)
-}
-
 const Comment = ({ text, createdAt }: Props) => {
-  const content = isValidWebUrl(text) ? (
-    <ReactTinyLink
-      cardSize="small"
-      showGraphic={true}
-      // maxLine={2}
-      minLine={1}
-      url={text}
-    />
-  ) : (
-    <Text>{text}</Text>
-  )
-
   return (
     <CommentWrapper>
       <PostedDate>
@@ -56,7 +43,7 @@ const Comment = ({ text, createdAt }: Props) => {
           ? `Posted on ${format(new Date(createdAt.toDate()), 'yyyy-MM-dd')}`
           : 'Loading'}
       </PostedDate>
-      {content}
+      <Linkify>{text}</Linkify>
     </CommentWrapper>
   )
 }
