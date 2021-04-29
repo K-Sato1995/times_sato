a9bca7731f22b8b1c18fb4fb3c87713319dbeca8
Katsuki <Katsuki_Sato0130@icloud.com>
2021-04-30 08:26:04 +0900

    Fix Header layout

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -4,36 +4,35 @@ import { Heading, Button } from 'components/atoms'
 import { auth } from 'firebaseConfig'
 
 const NavWrapper = styled.div`
-  margin: 0 auto;
-  height: 50px;
+  display: flex;
+  justify-content: center;
+  height: 3.125rem;
   border-bottom: solid ${(props) => props.theme.borderColor} 1px;
 `
 
 const Nav = styled.div`
-  margin: 0 auto;
+  width: 100%;
   max-width: 860px;
-  height: 50px;
+  height: 3.125rem;
+`
+
+const NavItems = styled.div`
+  padding: 0 2.25rem;
   display: flex;
   justify-content: space-between;
+
+  @media screen and (max-width: 29.9999em) {
+    padding: 0 0.625rem;
+  }
 `
 
 const Title = styled(Heading).attrs(() => ({
   size: 'h1',
-}))`
-  padding-left: 20px;
-
-  @media screen and (max-width: 29.9999em) {
-    padding-left: 10px;
-  }
-`
+}))``
 
 const SignOutLink = styled(Button)`
   font-size: 1rem;
-  padding-right: 20px;
 
-  @media screen and (max-width: 29.9999em) {
-    padding-right: 10px;
-  }
   :hover {
     color: #111;
   }
@@ -43,12 +42,14 @@ const Header = () => {
   return (
     <NavWrapper>
       <Nav>
-        <Title>times_sato</Title>
-        {auth.currentUser && (
-          <SignOutLink buttonType="link" onClick={() => auth.signOut()}>
-            Sign Out
-          </SignOutLink>
-        )}
+        <NavItems>
+          <Title>times_sato</Title>
+          {auth.currentUser && (
+            <SignOutLink buttonType="link" onClick={() => auth.signOut()}>
+              Sign Out
+            </SignOutLink>
+          )}
+        </NavItems>
       </Nav>
     </NavWrapper>
   )
