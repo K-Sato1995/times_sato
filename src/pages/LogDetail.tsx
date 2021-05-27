2c6c1881188d179ebe42303275a7ffd8bd8a7f46
Katsuki <Katsuki_Sato0130@icloud.com>
2021-05-27 19:15:52 +0900

    Show heat map on logDetail

--- src/pages/LogDetail.tsx
+++ src/pages/LogDetail.tsx
@@ -9,8 +9,13 @@ import {
 import { Heading, ContentWrapper } from 'components/atoms'
 import { LoadingState } from 'components/molecules'
 import { NewLogForm, LogBox } from 'components/organisms'
+import { format } from 'date-fns'
+import ReactTooltip from 'react-tooltip'
+import CalendarHeatmap from 'react-calendar-heatmap'
+import 'react-calendar-heatmap/dist/styles.css'
 
 const LogsConatiner = styled.div`
+  margin-top: 1rem;
   border: solid ${(props) => props.theme.borderColor} 1px;
   border-bottom: 0;
 `
@@ -45,10 +50,33 @@ const LogDetail = () => {
     sum += Number(log.hours)
   })
 
-  console.log(sum)
+  const formattedLogs: any = logs?.map((log) => {
+    const date = format(new Date(log.date.toDate()), 'yyyy-MM-dd')
+    return { date: date, count: log.hours }
+  })
+
+  const getTooltipDataAttrs = (value: { date: string; count: number }) => {
+    // Temporary hack around null value.date issue
+    if (!value || !value.date) {
+      return null
+    }
+    // Configuration for react-tooltip
+    return {
+      'data-tip': `${value.date.slice(0, 10)} | ${value.count} h`,
+    }
+  }
+
   return (
     <ContentWrapper>
       <Heading size={'h1'}>{logItem?.name}</Heading>
+
+      <ReactTooltip />
+      <CalendarHeatmap
+        startDate={new Date('2021-05-01')}
+        endDate={new Date('2022-05-01')}
+        values={formattedLogs}
+        tooltipDataAttrs={getTooltipDataAttrs}
+      />
 
       <LogsConatiner>
         {logs?.map((log) => (
