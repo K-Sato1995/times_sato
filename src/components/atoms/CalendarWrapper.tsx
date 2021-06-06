098d8f8ad89740d9d4be1227940fb11fe37b6048
Katsuki <Katsuki_Sato0130@icloud.com>
2021-06-06 17:38:44 +0900

    Refactor calendar wrapper

--- src/components/atoms/CalendarWrapper.tsx
+++ src/components/atoms/CalendarWrapper.tsx
@@ -4,42 +4,42 @@ const CalendarWrapper = styled.div`
   .fc {
     box-sizing: border-box;
     max-height: 600px;
-  }
 
-  .fc .fc-toolbar.fc-header-toolbar {
-    margin-bottom: 0.8rem;
-  }
-
-  .fc .fc-toolbar.fc-footer-toolbar {
-    margin-top: 0.8rem;
-  }
-
-  .fc .fc-toolbar-title {
-    font-size: 1.7rem;
-  }
-
-  .fc .fc-button-primary {
-    background-color: #fff;
-    border: none;
-    box-shadow: none;
-    outline: none;
-    color: #212329;
-
-    :focus,
-    :not(:disabled):active:focus,
-    :not(:disabled):active,
-    :hover,
-    :not(:disabled).fc-button-active,
-    :not(:disabled).fc-button-active:focus {
-      background-color: #fff;
-      color: #212329;
-      box-shadow: none;
-      outline: none;
-      border-radius: 0;
+    .fc-toolbar.fc-header-toolbar {
+      margin-bottom: 0.8rem;
     }
 
-    :not(:disabled).fc-button-active {
-      border-bottom: solid 2px #212329;
+    .fc-toolbar.fc-footer-toolbar {
+      margin-top: 0.8rem;
+    }
+
+    .fc-toolbar-title {
+      font-size: 1.7rem;
+    }
+
+    .fc-button-primary {
+      background-color: #fff;
+      border: none;
+      box-shadow: none;
+      outline: none;
+      color: #212329;
+
+      :focus,
+      :not(:disabled):active:focus,
+      :not(:disabled):active,
+      :hover,
+      :not(:disabled).fc-button-active,
+      :not(:disabled).fc-button-active:focus {
+        background-color: #fff;
+        color: #212329;
+        box-shadow: none;
+        outline: none;
+        border-radius: 0;
+      }
+
+      :not(:disabled).fc-button-active {
+        border-bottom: solid 2px #212329;
+      }
     }
   }
 `
