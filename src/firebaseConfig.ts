20f73b35b38a6e9560d1ffabd35dc8d531bf29a3
K-Sato <32632542+K-Sato1995@users.noreply.github.com>
2021-09-20 16:34:05 +0900

    Upgrade firebase version for auth

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -1,15 +1,17 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/firestore'
 
-firebase.initializeApp({
+import { getAuth } from 'firebase/auth'
+
+const firebaseApp = firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
 })
 
-const auth = firebase.auth()
 const firestore = firebase.firestore()
+
+const auth = getAuth(firebaseApp)
 
 export { auth, firebase, firestore }
